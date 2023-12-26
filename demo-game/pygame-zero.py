01cc84685f3fcf7685cf56e089e1098aef5c8616
TienMA <tienma@kaopiz.com>
2023-12-26 10:18:11 +0700

    fix name brick, add bullet image, create ability character

--- demo-game/pygame-zero.py
+++ demo-game/pygame-zero.py
@@ -1,13 +1,54 @@
 # type: ignore
 import pgzrun
 
-WIDTH = 200
-HEIGHT = 200
+WIDTH = 800
+HEIGHT = 600
 
 TITLE = "CHÀO MỪNG CÁC BẠN ĐẾN VỚI PYGAME ZERO"
-RECT = Rect((10, 10), (80, 80))
+DIRECTION_RUN = 4
+
+actor_1 = Actor('mario_run_1')
+list_bricks = []
+
+def actor_run():
+    global DIRECTION_RUN
+    if keyboard.up:
+        DIRECTION_RUN = -4
+        actor_1.y += DIRECTION_RUN
+    if keyboard.down:
+        DIRECTION_RUN = 4
+        actor_1.y += DIRECTION_RUN
+    if keyboard.left:
+        DIRECTION_RUN = -4
+        actor_1.x += DIRECTION_RUN
+    if keyboard.right:
+        DIRECTION_RUN = 4
+        actor_1.x += DIRECTION_RUN
+
+def on_key_down():
+    if keyboard.space:
+        create_brick()
+        
+
+def create_brick():
+    global list_bricks
+    list_bricks.append(Actor('brick_small', actor_1.pos))
+    
+def draw_bricks():
+    for brick in list_bricks:
+        brick.draw()
+
+def bricks_fly():
+    for brick in list_bricks:
+        brick.x += 4
 
 def draw():
-    screen.draw.textbox("Chào mừng các bạn", RECT)
+    screen.clear()
+    actor_1.draw()
+    draw_bricks()
+    
+def update():
+    actor_run()
+    bricks_fly()
     
 pgzrun.go()
