c0f8c4a2b16e148039b2491a8bbf1acf0bf24d17
Mai Anh Tien <tienham1610@gmail.com>
2024-04-10 22:27:21 +0700

    update code 10-04-2024

--- demo-game/pygame-zero-2.py
+++ demo-game/pygame-zero-2.py
@@ -6,16 +6,9 @@ WIDTH = 400
 HEIGHT = 200
 
 TITLE = "CHÀO MỪNG CÁC BẠN ĐẾN VỚI PYGAME ZERO"
-
-apple = Actor('apple-50-50', topleft=(0,0))
-
-def draw():
-    apple.draw()
-
-def becomeBrick():
-    animate(apple,top=200, duration=3)
     
-animate(apple, left=100, duration=3, on_finished=becomeBrick)
-
-
+def update():
+    if keyboard.Delete:
+        print("xoa")
+        
 pgzrun.go()
